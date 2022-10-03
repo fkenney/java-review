774bf6d154459b8355b0b34a07f824986f434f05
Felicia Kenney <fkenney@bravocg.com>
2022-10-03 11:37:08 -0400

    created classes

---
+++ src/main/java/com/hr/personnel/Employee.java
@@ -0,0 +1,33 @@
+package com.hr.personnel;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+  private String name;
+  private LocalDate hireDate;
+
+  public Employee() {
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDate getHireDate() {
+    return hireDate;
+  }
+
+  public void setHireDate(LocalDate hireDate) {
+    this.hireDate = hireDate;
+  }
+
+  public String getEmployeeInfo(){
+    return String.format("Name: %s , Hire Date:%s", getName(), getHireDate());
+  }
+}
